4052f7605afb5a3b8963ad89ea7e80efb6453f8a
David MacKintosh <dgawmackintosh@gmail.com>
2018-08-03 10:57:03 +0100

    Connects first action and reducer

--- src/components/posts_index.jsx
+++ src/components/posts_index.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
         return (
             <div>Posts index</div>
@@ -8,4 +15,4 @@ class PostsIndex extends Component {
     }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
