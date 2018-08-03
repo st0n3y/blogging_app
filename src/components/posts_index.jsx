5099df000fbfde6d1decce59be014de4b7567a72
David MacKintosh <dgawmackintosh@gmail.com>
2018-08-03 10:15:16 +0100

    Initial commit

---
+++ src/components/posts_index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+    render() {
+        return (
+            <div>Posts index</div>
+        );
+    }
+}
+
+export default PostsIndex;
