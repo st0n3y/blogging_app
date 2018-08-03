6f2153d8225f66d2f4f55065394f79606cd2c538
David MacKintosh <dgawmackintosh@gmail.com>
2018-08-03 16:32:16 +0100

    Starts building form

--- src/components/posts_new.jsx
+++ src/components/posts_new.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderField(field) {
+        return (
+            <div className='form-group'>
+                <label>{field.label}</label>
+                <input className='form-control' type='text' {...field.input} />
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div>Posts New</div>
+            <form>
+                <Field 
+                    label='Title'
+                    name='title'
+                    component={this.renderField} />
+                <Field 
+                    label='Tags'
+                    name='tags'
+                    component={this.renderField} />
+                <Field 
+                    label='Post Body'
+                    name='body'
+                    component={this.renderField} />
+            </form>
         );
    }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
