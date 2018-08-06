571ad0ac5bb7db826146746a48b978fa3c2b77c3
David MacKintosh <dgawmackintosh@gmail.com>
2018-08-06 10:57:16 +0100

    Wires up create post action

--- src/components/posts_new.jsx
+++ src/components/posts_new.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
@@ -21,7 +25,7 @@ class PostsNew extends Component {
     }
 
     onSubmit(values) {
-        console.log(values);
+        this.props.createPost(values);
     }
 
     render() {
@@ -42,6 +46,7 @@ class PostsNew extends Component {
                     name='body'
                     component={this.renderField} />
                 <button type='submit' className='btn btn-primary'>Submit</button>
+                <Link to='/' className='btn btn-danger'>Cancel</Link>
             </form>
         );
    }
@@ -60,4 +65,6 @@ function validate(values) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
