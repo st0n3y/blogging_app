a0f65b824eb72f0279e88ac273f30264809131d4
David MacKintosh <dgawmackintosh@gmail.com>
2018-08-06 12:44:33 +0100

    Allows deletion

--- src/components/posts_show.jsx
+++ src/components/posts_show.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { fetchPost } from '../actions';
+import { deletePost } from '../actions';
 
 class PostsShow extends Component {
     componentDidMount() {
-        const {id} = this.props.match.params;
-        this.props.fetchPost(id);
+        if(!this.props.post) {
+            const {id} = this.props.match.params;
+            this.props.fetchPost(id);
+        }
+    }
+
+    onDelete() {
+        const { id } = this.props.match.params;
+        this.props.deletePost(id, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -18,6 +29,12 @@ class PostsShow extends Component {
 
         return (
             <div>
+                <Link to='/'>Back to index</Link>
+                <button 
+                    className='btn btn-danger pull-xs-right'
+                    onClick={this.onDelete.bind(this)}>
+                    Delete Post
+                </button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
@@ -30,4 +47,4 @@ function mapStateToProps({ posts }, ownProps) {
     return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 
 import { FETCH_POSTS } from '../actions';
 import { FETCH_POST } from '../actions';
+import { DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
     switch(action.type) {
@@ -13,6 +14,8 @@ export default function(state = {}, action) {
             // newState[post.id] = post;
             // return newState;
             return { ...state, [action.payload.data.id]: action.payload.data };
+        case DELETE_POST:
+            return _.omit(state, action.payload);
         default: 
             return state;
     }
